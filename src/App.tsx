4e7d3ae561633f08e8d876b328aea973dc1e4374
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-10-04 21:08:30 -0300

    fix on code styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
-import { GlobalStyles } from './styles/global';
+import {Dashboard} from './components/Dashboard';
+import {Header} from './components/Header';
+import {GlobalStyles} from './styles/global';
 
 export function App() {
-  return (
-    <>
-      <Header />
-      <Dashboard />
-      <GlobalStyles />
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Dashboard/>
+            <GlobalStyles/>
+        </>
+    );
 }
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,13 +1,13 @@
-import { Summary } from '../Summary';
-import { TransactionsTable } from '../TransactionsTable';
+import {Summary} from '../Summary';
+import {TransactionsTable} from '../TransactionsTable';
 
-import { Container } from './styles';
+import {Container} from './styles';
 
 export function Dashboard() {
-  return (
-    <Container>
-      <Summary />
-      <TransactionsTable />
-    </Container>
-  );
+    return (
+        <Container>
+            <Summary/>
+            <TransactionsTable/>
+        </Container>
+    );
 }
